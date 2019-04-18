7e6636ce8ffa8e8a6a535e671df03fb81257700b
AkhilNewton <eminem.akhil17@gmail.com>
2019-04-18 11:35:47 +0530

    [NEWT] added R codes

---
+++ Akhil/R Codes/EX1_4.R
@@ -0,0 +1,6 @@
+a <- matrix(c(0,1,0,2,3,1), nrow=2,ncol=3, byrow = TRUE)
+b <- matrix(c(2,0,1,2,1,1), nrow=3,ncol=2, byrow = TRUE)
+c <- print(a %*% b)
+d <- print(b %*% a) 
+
+
---
+++ Akhil/R Codes/Exr1_34.R
@@ -0,0 +1,5 @@
+k<-1 #By providing value of first angle as PI
+y<-1 #By providing value of second angle as PI
+a <- matrix(c(cospi(k),sinpi(k),-sinpi(k), cospi(k)), nrow=2,ncol=2,byrow = TRUE)
+b <- matrix(c(cospi(y),sinpi(y),-sinpi(y), cospi(y)), nrow=2,ncol=2,byrow = TRUE)
+d <- print(a %*% b)
